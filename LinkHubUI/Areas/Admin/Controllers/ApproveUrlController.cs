edbd0b6bfdfb8054648a5a8c3c41b0c6d87852f0
kalel120 <imran_aust_cse@hotmail.com>
2016-08-07 00:10:46 +0600

    All the controllers and views are added

---
+++ LinkHubUI/Areas/Admin/Controllers/ApproveUrlController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Admin.Controllers {
+    public class ApproveUrlController : Controller {
+        // GET: Admin/ApproveUrl
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ LinkHubUI/Areas/User/Controllers/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using BOL;
+
+namespace LinkHubUI.Areas.User.Controllers {
+    public class UrlController : Controller {
+        // GET: User/Url
+        // This is for rendering the Index Page
+        public ActionResult Index() {
+            LinkHubDbEntities objDbEntities = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(objDbEntities.tbl_Category,"CategoryId","CategoryName");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tbl_Url objUrlForPost) {
+            throw new NotImplementedException();
+        }
+    }
+}
