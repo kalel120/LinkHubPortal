f9ba6eb925e063c1515953f270b81ea1e0011b8f
kalel120 <imran_aust_cse@hotmail.com>
2016-08-27 00:35:36 +0600

    Implementing Submit URL & Custom Validation is done

---
+++ BOL/UrlValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace BOL {
+    public class UrlValidation {
+        [Required(ErrorMessage = "Required")]
+        public string UrlTitle { get; set; }
+
+        [Required(ErrorMessage = "Required"), Url, UniqueUrl]
+        public string Url { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public string UrlDesc { get; set; }
+    }
+
+    public class UniqueUrlAttribute : ValidationAttribute {
+        protected override ValidationResult IsValid(object value, ValidationContext validContext) {
+            LinkHubDbEntities dbEntities = new LinkHubDbEntities();
+            var urlValue = value.ToString();
+            var count = dbEntities.tbl_Url.Where(x => x.Url == urlValue).ToList().Count;
+            if (count !=0) {
+                return new ValidationResult("Url Already Exists!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+
+    [MetadataType(typeof(UrlValidation))]
+    public partial class tbl_Url{}
+}
--- LinkHubUI/Areas/User/Controllers/UrlController.cs
+++ LinkHubUI/Areas/User/Controllers/UrlController.cs
@@ -1,19 +1,42 @@
 ﻿using System;
+using System.Linq;
 using System.Web.Mvc;
+using BLL;
 using BOL;
 
 namespace LinkHubUI.Areas.User.Controllers {
     public class UrlController : Controller {
-        // GET: User/Url
-        // This is for rendering the Index Page
+        private UrlBs objUrlBs;
+        private CategoryBs objCategoryBs;
+        private UserBs objUserBs;
+
+        public UrlController() {
+            objUserBs = new UserBs();
+            objCategoryBs = new CategoryBs();
+            objUrlBs = new UrlBs();
+        }
         public ActionResult Index() {
-            LinkHubDbEntities objDbEntities = new LinkHubDbEntities();
-            ViewBag.CategoryId = new SelectList(objDbEntities.tbl_Category,"CategoryId","CategoryName");
+            ViewBag.CategoryId = new SelectList(objCategoryBs.GetAllCategories().ToList(), "CategoryId", "CategoryName");
+            ViewBag.UserId = new SelectList(objUserBs.GetAllUser().ToList(), "UserId", "UserEmail");
             return View();
         }
         [HttpPost]
         public ActionResult Create(tbl_Url objUrlForPost) {
-            throw new NotImplementedException();
+            try {
+                if(ModelState.IsValid) {
+                    objUrlBs.Insert(objUrlForPost);
+                    TempData["Msg"] = "Created Successfully";
+                    return RedirectToAction("Index");
+                }
+                ViewBag.CategoryId = new SelectList(objCategoryBs.GetAllCategories().ToList(), "CategoryId", "CategoryName");
+                ViewBag.UserId = new SelectList(objUserBs.GetAllUser().ToList(), "UserId", "UserEmail");
+                return View("Index");
+            }
+            catch(Exception exception) {
+                TempData["Msg"] = "Failed" + exception;
+                return RedirectToAction("Index");
+                throw;
+            }
         }
     }
 }
