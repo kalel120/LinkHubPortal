696ca7f99da5bd932362decfacc017f3aa585b70
Al Imran <aimran@data-edge.com>
2016-09-04 17:00:30 +0600

    Change .NET Framework Target Type to 4.5.1

--- BOL/LinkHubDataModel.Designer.cs
+++ BOL/LinkHubDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\VS Projects\LinkHubUI\BOL\LinkHubDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects (VS)\LinkHubPortal\BOL\LinkHubDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
